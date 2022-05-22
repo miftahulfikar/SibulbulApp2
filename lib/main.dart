250ec07912b01e942a26f34fe9a435728db90166
miftahulfikar <miftahul.fikar@gmail.com>
2022-05-22 21:12:27 +0700

    step-4

--- lib/main.dart
+++ lib/main.dart
@@ -5,7 +5,6 @@ import 'package:newproject/views/login_view.dart';
 import 'package:newproject/views/register_view.dart';
 import 'package:newproject/views/verifyemail_view.dart';
 import 'firebase_options.dart';
-import 'dart:developer' as devtools show log;
 
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
@@ -19,6 +18,7 @@ void main() {
       routes: {
         '/login/': (context) => const LoginView(),
         '/register/': (context) => const RegisterView(),
+        '/notes/': (context) => const NotesView(),
       },
     ),
   );
--- lib/views/register_view.dart
+++ lib/views/register_view.dart
@@ -1,5 +1,6 @@
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'dart:developer' as devtools show log;
 
 class RegisterView extends StatefulWidget {
   const RegisterView({Key? key}) : super(key: key);
@@ -57,19 +58,19 @@ class _RegisterViewState extends State<RegisterView> {
               final email = _email.text;
               final password = _password.text;
               try {
-                final UserCredential =
+                final userCredential =
                     await FirebaseAuth.instance.createUserWithEmailAndPassword(
                   email: email,
                   password: password,
                 );
-                print(UserCredential);
+                devtools.log(userCredential.toString());
               } on FirebaseAuthException catch (e) {
                 if (e.code == 'weak-password') {
-                  print('password lemah');
+                  devtools.log('password lemah');
                 } else if (e.code == 'email-already-in-use') {
-                  print('email sudah terdaftar');
+                  devtools.log('email sudah terdaftar');
                 } else if (e.code == 'invalid-email') {
-                  print('format email tidak benar');
+                  devtools.log('format email tidak benar');
                 }
               }
             },
